5aba7bf9928aaa45ee1355564017215042e7e1a6
dived <270717147@163.com>
2013-04-23 17:46:15 +0800

    add function get_uid_by_pname

--- 11_ladder.py
+++ 11_ladder.py
@@ -44,7 +44,7 @@ class Fuck11(SGMLParser):
         SGMLParser.reset(self)
         self.urls = []
         self.view_state = ''
-        self.envent_validation = ''
+        self.event_validation = ''
         
     def start_a(self, attrs):
         pass
@@ -57,7 +57,7 @@ class Fuck11(SGMLParser):
             if dict_attrs['name']=='__VIEWSTATE':
                 self.view_state = dict_attrs['value']
             if dict_attrs['name']=='__EVENTVALIDATION':
-                self.envent_validation = dict_attrs['value']
+                self.event_validation = dict_attrs['value']
 #        self.view_state = [attrs['value'] for k,v in attrs if k=='name' and v=='__VIEWSTATE']
 #        self.envent_validation = [attrs['value'] for k,v in attrs if k=='name' and v=='__EVENTVALIDATION']
 
@@ -65,12 +65,14 @@ class Fuck11(SGMLParser):
 username = 'get_ladder ' #uid=347783110
 password = 'ladder123'
 time_stamp = int(time.time()*1000)
-u_name = '静夜思ing'
+u_name = 'turbogears' #
+u_name =  'honey丶轩'
 uid = 0
+request_url = 'http://i.5211game.com/request/?r=%s' % time_stamp 
 person_url = 'http://i.5211game.com/%s' % u_name
-ladder_url = 'http://i.5211game.com/rating/ladder?u=%s' % uid
+ladder_url = 'http://i.5211game.com/rating/ladder?u=%s' % uid  #旧版查询中心
 login_url = 'http://passport.5211game.com/t/Login.aspx'
-rating_url = 'http://i.5211game.com/request/rating/?r=%s' % time_stamp
+rating_url = 'http://i.5211game.com/request/rating/?r=%s' % time_stamp  #新版查询中心 
 
 class Ladder(object):
     ''''''
@@ -100,7 +102,7 @@ def login_platform():
         #fk11.view_state, fk11.envent_validation
         params = {}
         params['__VIEWSTATE'] = fk11.view_state
-        params['__EVENTVALIDATION'] = fk11.envent_validation
+        params['__EVENTVALIDATION'] = fk11.event_validation
         params['txtUser'] = username
         params['txtPassWord'] = password
         params['butLogin'] = '登陆'
@@ -129,7 +131,7 @@ def login_platform():
 
 def _post_rating_request(params):
     data = urllib.urlencode(params)
-    rating_request = Request(rating_url, data,)
+    rating_request = Request(rating_url, data)
     rating_response= urlopen(rating_request)
     result = rating_response.read()
     result = json.loads(result)
@@ -137,14 +139,25 @@ def _post_rating_request(params):
     return result
 
 
-def get_uid(u_name=''):
-    person_url = 'http://i.5211game.com/%s' % u_name
-    p_request = urlopen(person_url)
-    p_result = p_request.read()
-    print p_result
-    uid = re.findall(r"YY.d.j = (\d+)", p_result)[0]
+def get_uid_by_pname(u_name=u_name):
+#    person_url = 'http://i.5211game.com/%s' % u_name
+#    p_request = urlopen(person_url)
+#    p_result = p_request.read()
+#    print p_result
+#    uid = re.findall(r"YY.d.j = (\d+)", p_result)[0]
+    print '[*]XXX'
+    params = {}
+    params['method'] = 'getuidbypname'
+    params['name'] = u_name
+    data = urllib.urlencode(params)
+    uid_request = Request(request_url, data)
+    response = urlopen(uid_request)
+    result = json.loads(response.read())
+    if result['error']==1:
+        print result['reinfo']
+        return None
+    return int(result['uid'])
     
-    return int(uid)
 
 def get_ladder_score():
     
@@ -164,7 +177,7 @@ def get_ladder_score():
 #    result[''] = ''
 #    result[''] = ''
 #    result['uid'] = ''
-#    print result
+    print ladder_response.geturl()
     return ladder_response.geturl()#,
      
 
@@ -179,6 +192,7 @@ def load_score(user_id):
     return result
 
 def get_hero_score(user_id, hero_id):
+    """03.04.22 返回空值"""
     ps = {}
     ps['method'] = 'getscore'
     ps['uid'] = user_id
@@ -193,21 +207,23 @@ def get_hero_list(user_id):
     ps['u'] = user_id
     ps['t'] = 10001
     result = _post_rating_request(ps)
-#    print result
 #    rating_file = open('rating_%s.txt' % u_name, 'wb')
-    heroRoadInfos = str(result['heroRoadInfos'])
+    print result.keys()
+    heroRoadInfos = result['heroRoadInfos']
 #    rating_file.write(heroRoadInfos + '\n')
     ratingHeros = result['ratingHeros']
     '''{u'heroassist': 37, u'win': 0, u'resv10': 2, u'p_mvp': u'0.0', u'p_heroassist': u'6.2', u'total': 6, u'use': 6, u'resv9': 0, u'resv8': 0, u'resv5': 0,
      u'heroname': u'\u672b\u65e5\u4f7f\u8005', u'resv7': 1, u'resv6': 0, u'score': 467, u'heroId': u'UC42', u'offline': 0, u'p_resv6': u'0.0', u'p_resv7': 
      u'0.2', u'p_resv5': u'0.0', u'p_resv8': u'0.0', u'p_resv9': u'0.0', u'p_win': u'0.00%', u'mvp': 0, u'lost': 6, u'r_win': u'0.00%',
       u'herotype': u'\u529b\u91cf', u'p_resv10': u'0.3'}'''
-#    for hero in ratingHeros:
-    print 'heroRoadInfos', heroRoadInfos
+    for hero in ratingHeros:
+        print hero['heroname'], hero['score'], hero['total'], hero['r_win']
 #        rating_file.write(str(hero) + '\n')
-        
-#        print hero['heroname'], hero['heroId'], hero['score'], 'rating_match:',  hero['total'], 'win_rating:', hero['r_win'], '英魂次:', hero['resv10'], '场均英魂:', hero['p_resv10']
+#        for k,v in hero.items():
+#        print hero['heroname'], hero['heroId'], hero['score'], 'rating_match:',  hero['total'], 'win_rating:', hero['r_win'], '英魂次:', hero['resv10']
+        #, '场均英魂:', hero['p_resv10']
 #    rating_file.close()
+    print 'heroRoadInfos', heroRoadInfos
     
 def get_rating(user_id):
     ps = {}
@@ -216,27 +232,55 @@ def get_rating(user_id):
     ps['t'] = 10001
     result = _post_rating_request(ps)
     ttInfos = result['ttInfos']#天梯信息
-    mjInfos = result['mjInfos']#名将信息 就不分析了。
+#    mjInfos = result['mjInfos']#名将信息 就不分析了。
     rating = result['rating']#天梯积分 2200
     rank = result['rank']#排名1550
-    rankInfos = result['rankInfos']#排名前十的狗币。
+#    rankInfos = result['rankInfos']#排名前十的狗币。
+#    mjheroInfos = result['mjheroInfos']
+#    tm = result['tm'] 他妈32?
+    print result.keys()
+    if ttInfos is None:
+        print '这B号被封了'
+        return '这B号被封了'
+#    for  k, v in ttInfos.items():
+#        print k, v
+    print '总', ttInfos['Total']
+    print '胜', ttInfos['Win']
+    print '负', ttInfos['Lost']
+    print 'R_Win', ttInfos['R_Win']
+    print '天梯积分', rating
+    print '排名', rank
+#    print '排名前十', rankInfos
 #    for k,v in ttInfos.items():
 #        print k, ':', v
-    print '总场次:', ttInfos['Total']
-    print '胜率:', ttInfos['R_Win']
-    print '胜利:', ttInfos['Win']
-    print '失败:', ttInfos['Lost'] 
+#    print '总场次:', ttInfos['Total']
+#    print '胜率:', ttInfos['R_Win']
+#    print '胜利:', ttInfos['Win']
+#    print '失败:', ttInfos['Lost'] 
     print '逃跑率:', ttInfos['OfflineFormat']
 #    
 "YY.d.u = 347783110,YY.d.n = '二_十_五',YY.d.j = 333436659,YY.d.k = 'Turbogears' ,YY.wrt = '/',YY.page = 'rating';"
 "YY.d.u = 333436659,YY.d.n = 'turbogears',YY.d.j = YY.d.u,YY.d.k = YY.d.n ,YY.wrt = '/',YY.page = 'rating';"
 import re
 
-#print get_uid()
 get_ladder_score()
-#get_rating(get_uid())
-get_hero_list(get_uid(u'静夜思ing'))
-print 'end'
+uid = get_uid_by_pname()
+if uid!=None:
+    get_rating(uid)
+    get_hero_list(uid)
+#计算最近100盘天梯加分-0分-扣分
+#l = [2182, 2185, 2182, 2184, 2186, 2187, 2190, 2195, 2198, 2196, 2192, 2193, 2189, 2184, 2188, 2187, 2190, 2192, 2190, 2188, 2190, 2188, 2193, 2195, 2194, 2195, 2199, 2203, 2203, 2203, 2207, 2207, 2206, 2208, 2212, 2211, 2216, 2219, 2218, 2220, 2214, 2212, 2216, 2208, 2210, 2209, 2206, 2208, 2208, 2209, 2210, 2212, 2205, 2205, 2204, 2209, 2213, 2217, 2214, 2215, 2215, 2217, 2218, 2218, 2220, 2221, 2220, 2214, 2213, 2213, 2216, 2216, 2210, 2210, 2210, 2210, 2210, 2210, 2210, 2211, 2211, 2210, 2211, 2211, 2211, 2215, 2216, 2215, 2208, 2208, 2209, 2205, 2206, 2206, 2207, 2207, 2208, 2214, 2213, 2204]
+#win = 0
+#lost = 0
+#draw = 0
+#for i in range(0, len(l)-1):
+#    if l[i]<l[i+1]:
+#        win += 1
+#    elif l[i]==l[i+1]:
+#        draw += 1
+#    else:
+#        lost +=1
+print 'end'#, lost, win, draw
 #a = urllib.urlopen("http://www.baidu.com/img/shouye_b5486898c692066bd2cbaeda86d74448.gif")
 #f = open('bd.gif', 'wb')
 #f.write(a.read())
